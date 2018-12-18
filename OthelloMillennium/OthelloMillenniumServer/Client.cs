1e0a368a78d58b0e299e7762bbec039379aa4c8f
Maering <salomon.segan@gmail.com>
2018-12-18 16:41:45 +0100

    Initial commit
    This commit does not provide any working module. Used to get files from an other computer

---
+++ OthelloMillennium/OthelloMillenniumServer/Client.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+
+namespace OthelloMillenniumServer
+{
+    public class Client
+    {
+        #region Properties
+        public TcpClient TcpClient { get; private set; }
+        #endregion
+
+        private Client(ref TcpClient tcpClient)
+        {
+            TcpClient = tcpClient;
+        }
+
+        private Client(string hostname, int port)
+        {
+            if (string.IsNullOrEmpty(hostname))
+                throw new ArgumentException("Given hostname is invalid !");
+
+            if (port < 0)
+                throw new ArgumentException("Given port is invalid !");
+
+            // Init client
+            TcpClient = new TcpClient(hostname, port);
+        }
+
+        public static Client FromTCPClient(ref TcpClient tcpClient)
+        {
+            return new Client(ref tcpClient);
+        }
+
+        public static Client FromHostname(string hostname, int port)
+        {
+            return new Client(hostname, port);
+        }
+    }
+}
---
+++ OthelloMillennium/OthelloMillenniumServer/TCPServer.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OthelloMillenniumServer
+{
+    class TCPServer
+    {
+        public static int Port = 65432;
+
+        #region Singleton
+        private static readonly object padlock = new object();
+        private static TCPServer instance = null;
+
+        public static TCPServer Instance
+        {
+            get
+            {
+                if(instance == null)
+                {
+                    lock(padlock)
+                    {
+                        instance = new TCPServer();
+                    }
+                }
+                return instance;
+            }
+        }
+
+        private TCPServer() { }
+        #endregion
+
+        #region Properties
+        private readonly TcpListener listener = new TcpListener(IPAddress.Any, Port);
+        private readonly List<Client> clients = new List<Client>();
+        #endregion
+
+        // Thread
+        public static ManualResetEvent allDone = new ManualResetEvent(false);
+
+        public bool StartListening()
+        {
+            try
+            {
+                // Start to listen
+                listener.Start(100);
+
+                while (true)
+                {
+                    // Set the event to nonsignaled state.  
+                    allDone.Reset();
+
+                    // Start an asynchronous socket to listen for connections.  
+                    Console.WriteLine("Waiting for a connection...");
+
+                    // Accept connection
+                    listener.BeginAcceptTcpClient(new AsyncCallback(AcceptCallback), listener);
+
+                    // Wait until a connection is made before continuing.  
+                    allDone.WaitOne();
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(e.ToString());
+            }
+
+            return true;
+        }
+
+        public static void AcceptCallback(IAsyncResult ar)
+        {
+            // Signal the main thread to continue.  
+            allDone.Set();
+
+            // Register client
+            TcpClient client = instance.listener.EndAcceptTcpClient(ar);
+            instance.clients.Add(Client.FromTCPClient(ref client));
+        }
+
+        public static void ReadCallback(IAsyncResult ar)
+        {
+            string content = string.Empty;
+
+            // Retrieve the state object and the handler socket  
+            // from the asynchronous state object.  
+            StateObject state = (StateObject)ar.AsyncState;
+            Socket handler = state.workSocket;
+
+            // Read data from the client socket.   
+            int bytesRead = handler.EndReceive(ar);
+
+            if (bytesRead > 0)
+            {
+                // There  might be more data, so store the data received so far.  
+                state.sb.Append(Encoding.ASCII.GetString(
+                    state.buffer, 0, bytesRead));
+
+                // Check for end-of-file tag. If it is not there, read   
+                // more data.  
+                content = state.sb.ToString();
+                if (content.IndexOf("<EOF>") > -1)
+                {
+                    // All the data has been read from the   
+                    // client. Display it on the console.  
+                    Console.WriteLine("Read {0} bytes from socket. \n Data : {1}",
+                        content.Length, content);
+                    // Echo the data back to the client.  
+                    Send(handler, content);
+                }
+                else
+                {
+                    // Not all data received. Get more.  
+                    handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
+                    new AsyncCallback(ReadCallback), state);
+                }
+            }
+        }
+
+        private static void Send(Socket handler, String data)
+        {
+            // Convert the string data to byte data using ASCII encoding.  
+            byte[] byteData = Encoding.ASCII.GetBytes(data);
+
+            // Begin sending the data to the remote device.  
+            handler.BeginSend(byteData, 0, byteData.Length, 0,
+                new AsyncCallback(SendCallback), handler);
+        }
+
+        private static void SendCallback(IAsyncResult ar)
+        {
+            try
+            {
+                // Retrieve the socket from the state object.  
+                Socket handler = (Socket)ar.AsyncState;
+
+                // Complete sending the data to the remote device.  
+                int bytesSent = handler.EndSend(ar);
+                Console.WriteLine("Sent {0} bytes to client.", bytesSent);
+
+                handler.Shutdown(SocketShutdown.Both);
+                handler.Close();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        public static int Main(String[] args)
+        {
+            StartListening();
+            return 0;
+        }
+    }
+}
